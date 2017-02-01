c93a8013aad4ac529ff43bd500c004f60f360416
mirman-Gavin <gagoldsmith2019@mirmanmustangs.org>
2017-02-01 14:08:07 -0800

    Update 6

--- 6-from-dusk-til-done/src/index.js
+++ 6-from-dusk-til-done/src/index.js
@@ -1,24 +1,10 @@
 import $ from "jquery";
 import todoListItem from "./todoListItem";
+import newTodoForm from "./newTodoForm";
 var todos;
 
-function getTodos(callback) {
-  return $.ajax(
-    "/todos",
-    {
-      type: "GET"
-    }
-  )
-  .done( (data) => {
-    // This is bad and I should feel bad for writing it. Why?
-    console.log("success!")
-    callback(data);
-  })
-  .fail( () => {
-    console.error("Something bad happened");
-  });
-}
 
+deleteTodo()
 
 $(document).ready( () => {
     getTodos((data) => {
@@ -27,6 +13,9 @@ $(document).ready( () => {
         $("#root").append(
           todoListItem(todo)
         );
-      })
+      });
+      $("#root").append(
+        newTodoForm()
+      )
   });
 })
--- 6-from-dusk-til-done/src/todoListItem.js
+++ 6-from-dusk-til-done/src/todoListItem.js
@@ -1,4 +1,5 @@
 import $ from "jquery";
+
 export default function todoListitem(todo) {
   return $(document.createElement("div"))
     .append(
@@ -9,7 +10,25 @@ export default function todoListitem(todo) {
     .append(
       $(document.createElement("input"))
       .attr("type","checkbox")
+      .attr("id",`todo-${todo.id}-completed`)
       .attr("checked", todo.completed)
+      click((e) => {
+        const editedTodo = {
+          id: todo.id,
+          text: todo.text,
+          date: todo.date,
+          completed: e.target.checked
+        }
+                editTodo([],todo.id,editedTodo);
+      })
+    )
+    .append(
+      $(document.createElement("button"))
+      .attr("type","button")
+      .text("Delete")
+      .click(() => {
+        deleteTodo([],todo.id);
+      })
     )
     .append(
       $(document.createElement("input"))
@@ -17,6 +36,7 @@ export default function todoListitem(todo) {
       .attr("placeholder","mm/dd/yyyy")
       .attr("id","dates")
     )
+    .attr("id",`todo-${todo.id}`)
     .addClass("todo");
 
 }
